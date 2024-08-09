4d98deb8f16c34582cf2c2f19c8bd6309c978b20
Hamza <hamzaravani4@gmail.com>
2024-08-09 12:10:23 +0530

    changed cors to local

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: ["https://quiz-client-10.onrender.com/","http://localhost:5173/"],
+        origin: ["http://localhost:5173/"],
         methods: ["GET","POST"],
         credentials: true
     }
